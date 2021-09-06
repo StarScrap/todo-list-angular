be50b7a3812b2a5c0e13ea5bbc43a20bbd14a890
StarScrap <67431344+StarScrap@users.noreply.github.com>
2021-09-06 17:06:58 +0200

    todo-list-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Lista Zadań';
-
-  get footer(): string{
-    return ' 2020 © Lista zadań,All rights reserved.';
-  }
-}
+export class AppComponent {}
